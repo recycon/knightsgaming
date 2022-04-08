36831eb8214bbe889cdad2c8ec5350b5039dee11
recycon <you@example.commarioespaillat@knights.ucf.edu>
2022-04-08 15:04:45 -0400

    Co-authored-by: OmariHop <OmariHop@users.noreply.github.com>

--- src/App.js
+++ src/App.js
@@ -1,23 +1,39 @@
 import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div clasName='HomePage'>
+        <h3>Home</h3> 
+      </div>
+    <div className='register'>
+      <h1>Register</h1>
+    </div>
+    
+    <div>
+      <div className='registerBox'>
+        <h3 className='KnightsGame'>KnightsGame</h3>
+
+        <div className='email'>
+          <h3>Please Enter an email adress </h3>
+        <input type="email"/>
+        </div>
+        
+        <div className='passWord'>
+          <h3>Please Enter a password </h3>
+        <input type="text"/>
+        </div>
+       
+      </div>
+
+      
+    </div>
+    
+    
     </div>
   );
 }
---
+++ src/component/AboutUs.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+
+function AboutUs(){
+    return (
+        <div>
+           <button className="AboutUsBtn">About Us</button>
+        </div>
+    )
+}
+
+export default AboutUs;
