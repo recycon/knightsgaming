4334bb2e5cec3aa254a566effccb6d95b3a97b51
recycon <you@example.commarioespaillat@knights.ucf.edu>
2022-04-15 15:36:19 -0400

    Co-authored-by: OmariHop <OmariHop@users.noreply.github.com>

--- src/index.js
+++ src/index.js
@@ -4,10 +4,11 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import LoginPage from './pages/login';
+import Games from './pages/games'
 
 ReactDOM.render(
   <React.StrictMode>
-    <LoginPage />
+    <Games />
   </React.StrictMode>,
   document.getElementById('root')
 );
---
+++ src/pages/games.js
@@ -0,0 +1,48 @@
+import React from "react";
+import "./gg.css";
+import AboutUs from "../component/AboutUs";
+
+
+function Games(){
+
+
+    
+    
+
+    return (
+
+        <div className="yo">
+
+    
+        <div className="OverlappingImg">
+            
+        
+
+        <nav >
+            
+            <h3 className="nav-bar" id="KH"> KnightsGame</h3>
+            <input   className="nav-bar"type="search"/>
+            <h3 className="nav-bar">Bookmarked</h3>
+            <h3 className="nav-bar">Caterogies</h3>
+            
+
+
+
+        </nav>
+
+        <div className="Box">
+            <div className="littleBox">
+                <h4>name</h4>
+                <h4>Rating</h4>
+            </div>
+        </div>
+
+        
+        </div>
+        </div>
+    
+    )
+
+}
+
+export default Games;
